9edccd612808198fbc8d458172a377fd4c65c1d0
marcomanelli <tronmsi@gmail.com>
2021-04-22 18:05:37 +0200

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,13 @@ var todoArr = ['Spesa', 'Cucinare', 'Cucire', 'Telefonare']
 $(function() {
 
   for(var i=0; i<todoArr.length; i++) {
-    var clone = $('.todo-line').clone()
-
     $('.todo-box').append('<li><h5>' + todoArr[i] + '</h5></li>')
   }
+
+  $('input').keyup(function(event) {
+    if(event.which == 13) {
+      var clone = $('ul li').clone()
+
+    }
+  })
 })
